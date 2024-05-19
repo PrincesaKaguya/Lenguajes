1605926ed8d06d095fde4598720b902bc58581bc
PrincesaKaguya <107221616+PrincesaKaguya@users.noreply.github.com>
2024-05-19 10:34:46 -0600

    Add files via upload

---
+++ Ejercicio1_POO_lenguajes/src/ejercicio1_poo_lenguajes/EventoSimple.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio1_poo_lenguajes;
+
+/**
+ *
+ * @author zumba
+ */
+public class EventoSimple extends Evento {
+    public EventoSimple(String nombre, String fecha, String lugar) {
+        super(nombre, fecha, lugar);
+    }
+}
